d465d58a7d18b335be64512b879563791590016d
Dev Sarangdhar <devsarangdhar@gmail.com>
2026-08-23 21:29:51 +0530

    Time: 8 ms (53.28%) | Memory: 46.9 MB (64.23%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?')
+                leftQ++;
+            else
+                leftSum += num.charAt(i) - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?')
+                rightQ++;
+            else
+                rightSum += num.charAt(i) - '0';
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = rightQ - leftQ;
+
+        if (qDiff % 2 != 0)
+            return true;
+
+        return diff != (qDiff / 2) * 9;
+    }
+}
